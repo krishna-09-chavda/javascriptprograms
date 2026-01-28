f6cec50db6880ac1a320ab1fa66593138c7d17d3
unknown <krishnachavda903@gmail.com>
2026-01-28 13:25:07 +0530

    [krc][data types vaeriables files are added]

---
+++ datatypes.js
@@ -0,0 +1,188 @@
+// // // // // two types of data type
+// // // // // 1. primitive 
+// // // // //Number
+// // // // //BigInt
+// // // // //String
+// // // // //boolean
+// // // // //Symbol
+// // // // //null
+// // // // //undefine
+
+// // // // // 2 Non-primitive
+// // // // //array
+// // // // //object
+// // // // //function
+
+
+// // // // //let num = 1;
+// // // // //let num2 = 2;
+
+// // // // //console.log("num",num,num2);
+
+// // // // let name="krishna";
+// // // // let name2="hetvi";
+
+// // // // console.log("names:",name2,name);
+
+// // // // //boolean
+// // // // let isTrue=true;
+// // // // let isFalse=false;
+// // // // console.log("boolean:",isTrue);
+
+// // // // // undefined
+
+// // // //  let a;
+// // // //  console.log("undefined ", a);
+
+
+
+// // // //  let bigInt= 12343455465465757n
+
+
+// // // // console.log("bigint:",bigInt);
+
+
+// // // // let student={
+// // // //     name : "krishna",
+// // // //     roll : 65,
+// // // //     address:{
+// // // //         city:"ksd",
+// // // //     }
+// // // // }
+// // // // console.log(student);
+
+
+
+// // // // //Array
+// // // // let array =[1,2,3,4,5,6,7.8];
+
+// // // // console.log(a);
+
+
+// // // //function
+
+// // // function greet()
+// // // {
+// // //     console/log("this is function");
+
+// // // }
+// // // greet();
+
+
+
+// // // function  showValues(value){
+// // //    console.log("this is values :",value);
+
+// // //    if (value==20){
+// // //     console.log("number-------");
+// // //    }
+
+// // //  }
+
+
+// // // showValues();
+// // // showValues(20);
+// // // showValues([1,2,3,4,5]);
+
+
+// // // function sum(num1,num2){
+// // //     console.log("sum is:",num1+num2);
+// // // }
+// // // sum(7,7);
+
+// // const add=(num1,num2)=>{
+// //     return num1+num2
+// // }
+// // console.log("this is arrow function is :",add(12,12))
+
+
+// // // function substraction (num1,num2)
+// // // {
+// // //     console.log("substraction is:",num1-num2)
+
+// // // }
+// // // substraction(10,5);
+
+// // const substraction=(num1,num2)=>{
+// //     return num1-num2
+// // }
+// // console.log("this is substraction function  is :",substraction(10,5))
+
+// // // function multi (num1,num2)
+// // // {
+
+
+// // //    console.log("multi is:",num1*num2);
+
+// // // }
+// // // multi (10,5);
+
+// // const multi=(num1,num2)=>{
+// //     return num1*num2
+// // }
+// // console.log("this is multi function is :",multi(10,5))
+
+// // // function div(num1,num2)
+// // // {
+// // //     console.log("div is:",num1/num2);
+// // // }
+// // // div(10,5);
+// // const div=(num1,num2)=>{
+// //     return num1/num2
+// // }
+// // console.log("this is div function is  :",div(10,5))
+
+
+
+
+
+// // let value = 10;
+// // console.log("defaulgt value:",value);
+
+// // value+=5
+// // console.log(value);
+// // value-=5
+// // console.log(value);
+// // value*5
+// // console.log(value);
+// // value/=5
+// // console.log(value);
+
+
+//  let age=20;
+//  let hasId = true
+
+//  let name="krishna";
+
+//  let obj={
+//     nmae:"krishna",
+ 
+//  rollno:10
+// }
+
+
+//  console.log("logical   AND (&&) :" ,age > 18 && hasId);
+// console.log("logical   OR(||) :" ,age > 18 || hasId);
+// console.log("logical   NOT(!)" , ! hasId);
+
+
+// console.log (typeof  age)
+// console.log (typeof  hasid)
+// console.log (typeof  name)
+// console.log (typeof  obj)
+
+
+let value=10;
+let num="10"
+
+console.log("value == num : ", value == num);
+console.log("value === num : ", value === num);
+console.log("value != num :", value != num);
+
+
+
+
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,16 @@
+//var a = 10;
+//console.log(a);
+
+
+//let a = 10;
+
+//if(true)
+//{
+   // let a=10;
+//}
+//console.log(a);
+
+const pi = 3.14;
+console.log(pi);
+
+
